a88202a8acc0c70428c806f0d8383d3ea111adc1
ctonnesen <christian.tonnesen@gmail.com>
2021-05-19 18:28:25 -0400

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
+export const KEY = '36f27c1a-c372-4ba1-a856-1974f51b6e05';
+export const MODAL_CLOSE_SEC = 2.5;
